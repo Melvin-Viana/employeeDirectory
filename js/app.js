354606bc0cd0486b801aa864bdaf4af6c7040657
Melvin Viana <mjviana057@gmail.com>
2019-05-23 19:43:05 -0700

    Generate random user data

--- js/app.js
+++ js/app.js
@@ -1,8 +1,34 @@
-// TODO: Fetch 12 Random users from the api
-const getUserJSON =fetch('https://randomuser.me/api/?results=12&nat=us').then(res=>res.json());
-const getRandomUsers = ()=> getUserJSON.then(e=>e.results);
+// Elements
+const container = document.querySelector('.container');
 
+// TODO: Fetch 12 Random users from the api
+async function fetchData(url){
+    const response = await fetch(url);
+    const data = await response.json();
+
+    return data;
+}
+fetchData('https://randomuser.me/api/?results=12&nat=us').then(e=>{e.results.forEach(e=>generateEmployeeInfo(e))});
 //TODO: New random employee information displays each time the page refreshes
+const generateEmployeeInfo = (userData)=>{
+    const div = document.createElement('div')
+    div.classList.add("employee-container");
+    const img = document.createElement('img');
+    img.classList.add('employee-picture');
+    img.setAttribute('src', userData.picture.large);
+    const name = document.createElement('p');
+    name.classList.add('employee-name');
+    name.innerHTML=`${userData.name.first} ${userData.name.last}`;
+    const email = document.createElement('p');
+    email.classList.add('employee-email');
+    email.innerHTML=userData.email;
+    div.append(img);
+    div.append(name);
+    div.append(email);
+    container.append(div);
+    // Show modal when clicked
+    // div.addEventListener('click',showModal(userData))
+}
 
 /*TODO: The directory includes the following:
     Employee Image
