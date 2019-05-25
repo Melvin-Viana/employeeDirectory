1523a4bac078e8f38c7421fc111291a22244693f
Melvin Viana <mjviana057@gmail.com>
2019-05-25 16:56:05 -0700

    Func for  switching employees

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 const container = document.querySelector('.directory-container'),
-    modal = document.querySelector('#modal-container'),
+    modalContainer = document.querySelector('#modal-container'),
     modalImage = document.querySelector('.modal-image'),
     modalName = document.querySelector('.name'),
     modalEmail = document.querySelector('.email'),
@@ -8,8 +8,11 @@ const container = document.querySelector('.directory-container'),
     modalAddress = document.querySelector('.address'),
     modalBirthday = document.querySelector('.birthday'),
     modalCity = document.querySelector('.location'),
-    close = document.querySelector('.close');
+    close = document.querySelector('.close'),
+    leftContainer = document.querySelector('.left-container'),
+    rightContainer = document.querySelector('.right-container');
 
+const userResults = [];
 // TODO: Fetch 12 Random users from the api
 async function fetchData(url) {
     const response = await fetch(url);
@@ -17,13 +20,11 @@ async function fetchData(url) {
 
     return data;
 }
-// Fetch users and display error on console if error occurs
-fetchData('https://randomuser.me/api/?results=12&nat=us').then(async e => {
-    await e.results.forEach((item, index) => generateEmployeeInfo(item, index))
-}).catch(e => console.log(e));
 
+let employeeIndex = 0;
 //TODO: New random employee information displays each time the page refreshes
-const generateEmployeeInfo = (userData) => {
+const generateEmployeeInfo = (userData, employeeIndex) => {
+    userResults.push(userData);
     // Create elements
     const div = createElement('div', "", "employee-container"),
         img = createElement("img", "", "employee-picture"),
@@ -42,7 +43,7 @@ const generateEmployeeInfo = (userData) => {
     div.append(dataContainer);
     container.append(div);
     // Show modal when clicked
-    div.addEventListener('click', () => showModal(userData))
+    div.addEventListener('click', () => showModal(userData, employeeIndex))
 }
 
 const createElement = (tag, innerHTML, className) => {
@@ -51,10 +52,8 @@ const createElement = (tag, innerHTML, className) => {
     elem.classList.add(className);
     return elem;
 }
-//** Employees can be filtered by name or username
-
 //Display Modal with selected userData
-const showModal = userData => {
+const showModal = (userData, index) => {
     const {
         email,
         phone,
@@ -75,22 +74,66 @@ const showModal = userData => {
         }
     } = userData;
     modalImage.setAttribute('src', userData.picture.large);
-    modal.style.display = "inline";
-    modal.style.animation = "fadeIn .5s";
+    modalContainer.style.display = "inline";
+    modalContainer.style.animation = "fadeIn .5s";
     modalName.innerHTML = `${first} ${last}`;
     modalEmail.innerHTML = email;
     modalPhone.innerHTML = phone;
     modalCity.innerHTML = city + ", " + state;
     modalAddress.innerHTML = street;
     modalBirthday.innerHTML = date;
+    employeeIndex = index;
+    (employeeIndex == 0) ? leftContainer.style.display = "none": leftContainer.style.display = "block";
+    (employeeIndex == 12) ? rightContainer.style.display = "none": rightContainer.style.display = "block";
 }
+
+// Fetch users and display error on console if error occurs
+fetchData('https://randomuser.me/api/?results=12&nat=us').then(async e => {
+    await e.results.forEach((item, index) => generateEmployeeInfo(item, index))
+}).catch(e => console.log(e)).finally(() => {
+    const modal = document.querySelector('.modal');
+    // Close
+    close.addEventListener('click', () => {
+        modalContainer.style.animation = "fadeOut .5s";
+        setTimeout(() => modalContainer.style.display = "none", 400);
+    });
+
+
 // ** Functionality has been added to switch back and forth between employees when the detail modal window is open.
 
+    // Left arrow previous user
+    leftContainer.addEventListener('click', () => {
+        if (employeeIndex == 0) {
+            return false;
+        }
 
-// Close
-close.addEventListener('click', () => {
-    modal.style.animation = "fadeOut .5s";
-    setTimeout(() => modal.style.display = "none", 400);
+        employeeIndex--;
+        modal.style.display = "none",
+            modal.style.animation = "fadeIn .5s";
+        modal.style.display = "flex",
+            showModal(userResults[employeeIndex], employeeIndex);
+            setTimeout(() => {
+                modal.style.animation = ""
+            }, 500);
+    });
+    // Right arrow next user
+    rightContainer.addEventListener('click', () => {
+        if (employeeIndex == 11) {
+            return false;
+        }
+        employeeIndex++;
+        modal.style.display = "none",
+        modal.style.animation = "fadeIn .5s";
+        modal.style.display = "flex",
+            showModal(userResults[employeeIndex], employeeIndex);
+
+        setTimeout(() => {
+            modal.style.animation = ""
+        }, 500);
+    });
+
 });
-    
-    document.querySelector('body').addEventListener('click', (e)=>{console.log(e.target)})
+
+
+
+//** Employees can be filtered by name or username
