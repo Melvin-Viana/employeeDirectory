bf6dd8e7484a5a7806c4d7d28361d1233e5765de
Melvin Viana <mjviana057@gmail.com>
2019-05-27 16:57:20 -0700

    Cleanup code

--- js/app.js
+++ js/app.js
@@ -11,18 +11,15 @@ const directoryContainer = document.querySelector('.directory-container'),
     close = document.querySelector('.close'),
     leftContainer = document.querySelector('.left-container'),
     rightContainer = document.querySelector('.right-container');
-
 const userResults = [];
-// TODO: Fetch 12 Random users from the api
+//* Fetch 12 Random users from the api
 async function fetchData(url) {
     const response = await fetch(url);
     const data = await response.json();
-
     return data;
 }
-
 let employeeIndex = 0;
-//TODO: New random employee information displays each time the page refreshes
+//* New random employee information displays each time the page refreshes
 const generateEmployeeInfo = (userData, employeeIndex) => {
     // Create elements
     const div = createElement('div', "", "employee-container"),
@@ -31,9 +28,7 @@ const generateEmployeeInfo = (userData, employeeIndex) => {
         email = createElement('p', userData.email, 'employee-email'),
         city = createElement('p', userData.location.city, 'employee-city'),
         dataContainer = createElement('div', '', 'user-data');
-
     img.setAttribute('src', userData.picture.large);
-
     // Append all elements
     div.append(img);
     dataContainer.append(name);
@@ -44,7 +39,6 @@ const generateEmployeeInfo = (userData, employeeIndex) => {
     // Show modal when clicked
     div.addEventListener('click', () => showModal(userData, employeeIndex))
 }
-
 const createElement = (tag, innerHTML, className) => {
     const elem = document.createElement(tag);
     elem.innerHTML = innerHTML;
@@ -75,15 +69,14 @@ const showModal = (userData, index) => {
     } = userData;
     // Convert UTC date to Date
     let birthDate = new Date(date);
-
-    // TODO: Format Address and Date
+    //* Format Address and Date
     modalImage.setAttribute('src', large);
     modalContainer.style.display = "inline";
     modalContainer.style.animation = "fadeIn .5s";
     modalName.innerHTML = `${first} ${last}`;
     modalEmail.innerHTML = email;
     modalPhone.innerHTML = phone;
-    modalCity.innerHTML = city ;
+    modalCity.innerHTML = city;
     modalAddress.innerHTML = `${street}, ${states_hash[state]} ${postcode} `;
     modalBirthday.innerHTML = `Birthday: ${getDate(birthDate)}`;
     employeeIndex = index;
@@ -91,7 +84,6 @@ const showModal = (userData, index) => {
     (employeeIndex == 0) ? leftContainer.style.display = "none": leftContainer.style.display = "block";
     (employeeIndex == 11) ? rightContainer.style.display = "none": rightContainer.style.display = "block";
 }
-
 
 // Fetch users and display error on console if error occurs
 fetchData('https://randomuser.me/api/?results=12&nat=us')
@@ -107,16 +99,12 @@ fetchData('https://randomuser.me/api/?results=12&nat=us')
             modalContainer.style.animation = "fadeOut .5s";
             setTimeout(() => modalContainer.style.display = "none", 400);
         });
-
-
-        // ** Functionality has been added to switch back and forth between employees when the detail modal window is open.
-
+        // * Functionality has been added to switch back and forth between employees when the detail modal window is open.
         // Left arrow previous user
         leftContainer.addEventListener('click', () => {
             if (employeeIndex == 0) {
                 return false;
             }
-
             employeeIndex--;
             modalSwitch();
         });
@@ -126,106 +114,99 @@ fetchData('https://randomuser.me/api/?results=12&nat=us')
                 return false;
             }
             employeeIndex++;
-           modalSwitch();
+            modalSwitch();
         });
-        //** Employees can be filtered by name or username
-
+        //* Employees can be filtered by name or username
         const aTozFilter = document.querySelector('.fName-az'),
             zToaFilter = document.querySelector('.fName-za');
-
-        aTozFilter.addEventListener('click',()=>filterCallback('name','first',false));
-
-        zToaFilter.addEventListener('click',()=>filterCallback('name','first',true));
-
+        aTozFilter.addEventListener('click', () => filterCallback('name', 'first', false));
+        zToaFilter.addEventListener('click', () => filterCallback('name', 'first', true));
     });
 
+// ===================================================================================
+// State object to convert to abbreviation
+const states_hash = {
+    "wyoming": "WY",
+    "wisconsin": "WI",
+    "west virginia": "WV",
+    "washington": "WA",
+    "virginia": "VA",
+    "virgin islands": "VI",
+    "vermont": "VT",
+    "utah": "UT",
+    "texas": "TX",
+    "tennessee": "TN",
+    "south dakota": "SD",
+    "south carolina": "SC",
+    "rhode island": "RI",
+    "puerto rico": "PR",
+    "pennsylvania": "PA",
+    "palau": "PW",
+    "oregon": "OR",
+    "oklahoma": "OK",
+    "ohio": "OH",
+    "northern mariana islands": "MP",
+    "north dakota": "ND",
+    "north carolina": "NC",
+    "new york": "NY",
+    "new mexico": "NM",
+    "new jersey": "NJ",
+    "new hampshire": "NH",
+    "nevada": "NV",
+    "nebraska": "NE",
+    "montana": "MT",
+    "missouri": "MO",
+    "mississippi": "MS",
+    "minnesota": "MN",
+    "michigan": "MI",
+    "massachusetts": "MA",
+    "maryland": "MD",
+    "marshall islands": "MH",
+    "maine": "ME",
+    "louisiana": "LA",
+    "kentucky": "KY",
+    "kansas": "KS",
+    "iowa": "IA",
+    "indiana": "IN",
+    "illinois": "IL",
+    "idaho": "ID",
+    "hawaii": "HI",
+    "guam": "GU",
+    "georgia": "GA",
+    "florida": "FL",
+    "federated states of micronesia": "FM",
+    "district of columbia": "DC",
+    "delaware": "DE",
+    "connecticut": "CT",
+    "colorado": "CO",
+    "california": "CA",
+    "arkansas": "AR",
+    "arizona": "AZ",
+    "american samoa": "AS",
+    "alaska": "AK",
+    "alabama": "AL"
+}
 const getDate = (date) => {
     return getMonthName(date.getMonth()) + " " + date.getDate();
 }
-
 const getMonthName = (month) => {
     const arr = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
     return arr[month];
 }
-// State object to convert to abbreviation
-const states_hash =
-    {
-        "wyoming": "WY",
-        "wisconsin": "WI",
-        "west virginia": "WV",
-        "washington": "WA",
-        "virginia": "VA",
-        "virgin islands": "VI",
-        "vermont": "VT",
-        "utah": "UT",
-        "texas": "TX",
-        "tennessee": "TN",
-        "south dakota": "SD",
-        "south carolina": "SC",
-        "rhode island": "RI",
-        "puerto rico": "PR",
-        "pennsylvania": "PA",
-        "palau": "PW",
-        "oregon": "OR",
-        "oklahoma": "OK",
-        "ohio": "OH",
-        "northern mariana islands": "MP",
-        "north dakota": "ND",
-        "north carolina": "NC",
-        "new york": "NY",
-        "new mexico": "NM",
-        "new jersey": "NJ",
-        "new hampshire": "NH",
-        "nevada": "NV",
-        "nebraska": "NE",
-        "montana": "MT",
-        "missouri": "MO",
-        "mississippi": "MS",
-        "minnesota": "MN",
-        "michigan": "MI",
-        "massachusetts": "MA",
-        "maryland": "MD",
-        "marshall islands": "MH",
-        "maine": "ME",
-        "louisiana": "LA",
-        "kentucky": "KY",
-        "kansas": "KS",
-        "iowa": "IA",
-        "indiana": "IN",
-        "illinois": "IL",
-        "idaho": "ID",
-        "hawaii": "HI",
-        "guam": "GU",
-        "georgia": "GA",
-        "florida": "FL",
-        "federated states of micronesia": "FM",
-        "district of columbia": "DC",
-        "delaware": "DE",
-        "connecticut": "CT",
-        "colorado": "CO",
-        "california": "CA",
-        "arkansas": "AR",
-        "arizona": "AZ",
-        "american samoa": "AS",
-        "alaska": "AK",
-        "alabama": "AL"
-    }
-
 // Sort Function
-const sortBy = (prop,nestedProp) => (prop1,prop2) => (prop1[prop][nestedProp]>prop2[prop][nestedProp])?1:-1 ;
+const sortBy = (prop, nestedProp) => (prop1, prop2) => (prop1[prop][nestedProp] > prop2[prop][nestedProp]) ? 1 : -1;
 // Callback function for filter click event
-const filterCallback =(prop1,nestedProp,reverse)=>{
+const filterCallback = (prop1, nestedProp, reverse) => {
     directoryContainer.innerHTML = "";
-    (reverse)?userResults.reverse(sortBy(prop1,nestedProp)):userResults.sort(sortBy(prop1,nestedProp));
+    (reverse) ? userResults.reverse(sortBy(prop1, nestedProp)): userResults.sort(sortBy(prop1, nestedProp));
     userResults.forEach((item, index) => generateEmployeeInfo(item, index));
-    }
-const modalSwitch = ()=>{
+}
+const modalSwitch = () => {
     const modal = document.querySelector('.modal');
     modal.style.display = "none",
-    modal.style.animation = "fadeIn .5s";
+        modal.style.animation = "fadeIn .5s";
     modal.style.display = "flex",
         showModal(userResults[employeeIndex], employeeIndex);
-
     setTimeout(() => {
         modal.style.animation = ""
     }, 500);
